ab24681fffe6a0770af2400eb0542b6c3f18a8af
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2016-08-11 01:01:23 -0400

    Add binding behavior and repeat.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ export class Welcome {
   heading = 'Code on the Beach Registration';
   firstName = 'John';
   lastName = 'Doe';
+  attendees = [];
 
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
 
   submit() {
+    this.attendees.push(this.fullName);
     alert(`Welcone to Code on the Beach, ${this.fullName}!`);
+
+    this.firstName = '';
+    this.lastName = '';
   }
 }
